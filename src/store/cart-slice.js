7e00c7ec9675bed453f642de4b71735f3c6c38ea
Hariharan Mohan <haranhari350@gmail.com>
2024-11-18 14:17:26 -0600

    Utilized redux in product and protuctItem components to add the items to cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -12,6 +12,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
 
       if (!existingItem) {
         state.items.push({
@@ -29,6 +30,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
