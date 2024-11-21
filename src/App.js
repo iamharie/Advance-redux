aa817c3269fae8f09d655ce6f7c47202eb5a7543
Hariharan Mohan <haranhari350@gmail.com>
2024-11-21 14:05:05 -0600

    Trail: Deleted, using useEffect() to send my http req based on the latest cart state by pulling them using useSelector() inside my App.js component, next move to add the fetch logic with have proper error catch

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCArt = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://e-commerce-7ecc8-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCArt && <Cart />}
